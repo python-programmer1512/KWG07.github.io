e8832f8e58b414f1e293f02a9487011d04b9b89b
KWG07 <joseph13ko15@gmail.com>
2024-03-12 01:35:02 +0900

    2024-03-12

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -2,6 +2,7 @@
   https://www.toptal.com/developers/cssminifier
   min.css 제작방법
 -->
+<link rel="stylesheet" href="style.css">
 <div class="content">
   <div class="container">
     <section class="main-text">
@@ -9,45 +10,53 @@
         <br>
         <span><b>Hi! I'm Wongyu Koh known as KWG07</b></span> 
     </section>
-    <section class="main-text">
-      <span class="title"><b>안녕하세요! 개발을 좋아하는 KWG07, 고원규 입니다!</b></span>
-      <br>
-      <span><b>Hi! I'm Wongyu Koh known as KWG07</b></span> 
-    </section>
-    <section class="main-text">
-      <span class="title"><b>안녕하세요! 개발을 좋아하는 KWG07, 고원규 입니다!</b></span>
-      <br>
-      <span><b>Hi! I'm Wongyu Koh known as KWG07</b></span> 
-    </section>
-    <section class="main-text">
-      <span class="title"><b>안녕하세요! 개발을 좋아하는 KWG07, 고원규 입니다!</b></span>
-      <br>
-      <span><b>Hi! I'm Wongyu Koh known as KWG07</b></span> 
-    </section>
-    <section class="main-text">
-      <span class="title"><b>안녕하세요! 개발을 좋아하는 KWG07, 고원규 입니다!</b></span>
-      <br>
-      <span><b>Hi! I'm Wongyu Koh known as KWG07</b></span> 
-    </section>
-    <section class="main-text">
-      <span class="title"><b>안녕하세요! 개발을 좋아하는 KWG07, 고원규 입니다!</b></span>
-      <br>
-      <span><b>Hi! I'm Wongyu Koh known as KWG07</b></span> 
-    </section>
-    <section class="main-text">
-      <span class="title"><b>안녕하세요! 개발을 좋아하는 KWG07, 고원규 입니다!</b></span>
-      <br>
-      <span><b>Hi! I'm Wongyu Koh known as KWG07</b></span> 
-    </section>
-    <section class="main-text">
-      <span class="title"><b>안녕하세요! 개발을 좋아하는 KWG07, 고원규 입니다!</b></span>
-      <br>
-      <span><b>Hi! I'm Wongyu Koh known as KWG07</b></span> 
-    </section>
-    <section class="main-text">
-      <span class="title"><b>안녕하세요! 개발을 좋아하는 KWG07, 고원규 입니다!</b></span>
-      <br>
-      <span><b>Hi! I'm Wongyu Koh known as KWG07</b></span> 
-    </section>
+    <div class="Section" id="log-1">
+      <section class="section-grid">
+        <div class="section-title">
+          <p>
+            EXAMPLE
+          </p>
+        </div>
+        <div class="text">
+          <p>
+            안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
+          </p>
+        </div>
+        <img class="image-scaling"  src="https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=9046601&filePath=L2Rpc2sxL25ld2RhdGEvMjAxNC8yMS9DTFM2L2FzYWRhbFBob3RvXzI0MTRfMjAxNDA0MTY=&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004" alt="" />
+
+      </section>
+    </div>
+    <div class="Section" id="log-2">
+      <section class="section-grid">
+        <div class="section-title">
+          <p>
+            EXAMPLE
+          </p>
+        </div>
+        <div class="text">
+          <p>
+            안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
+          </p>
+        </div>
+        <img class="image-scaling"  src="https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=9046601&filePath=L2Rpc2sxL25ld2RhdGEvMjAxNC8yMS9DTFM2L2FzYWRhbFBob3RvXzI0MTRfMjAxNDA0MTY=&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004" alt="" />
+     
+      </section>
+    </div>
+    <div class="Section" id="log-3">
+      <section class="section-grid">
+        <div class="section-title">
+          <p>
+            EXAMPLE
+          </p>
+        </div>
+        <div class="text">
+          <p>
+            안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요
+          </p>
+        </div>
+        <img class="image-scaling"  src="https://gongu.copyright.or.kr/gongu/wrt/cmmn/wrtFileImageView.do?wrtSn=9046601&filePath=L2Rpc2sxL25ld2RhdGEvMjAxNC8yMS9DTFM2L2FzYWRhbFBob3RvXzI0MTRfMjAxNDA0MTY=&thumbAt=Y&thumbSe=b_tbumb&wrtTy=10004" alt="" />
+     
+      </section>
+    </div>
   </div>
 </div>
